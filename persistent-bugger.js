c16be02bb58b434b11e312ecf557a1b3f701c40e
github username <samson.yuwono@gmail.com>
2018-06-24 12:42:03 -0400

    persistent-bugger completed

---
+++ persistent-bugger.js
@@ -0,0 +1,14 @@
+function persistence(num) {
+  let arr = num.toString().split("");
+  let multipliedCount = 0;
+  let product;
+  while (arr.length > 1) {
+    product = arr.reduce((a, b) => {
+      return a * b;
+    });
+    multipliedCount++;
+    arr = product.toString().split("");
+  }
+
+  return multipliedCount;
+}
